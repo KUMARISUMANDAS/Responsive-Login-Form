49712b90e4ae9f7a3717da6f0ea1ee8ba1d28ed4
KUMARYSUMANDAS <suman780310@gmail.com>
2025-09-07 01:19:03 +0530

    change this project and add

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import SocialLogin from "./components/SocialLogin";
+import InputField from "./InputField";
+
+const App = () => {
+  return (
+    <div className="Login-container">
+      <h2 className="form-title">Log In With</h2>
+      
+      <SocialLogin />
+
+      <p className="separator">
+        <span>or</span>
+      </p>
+
+      <form action="#" className="login-form">
+        <InputField type="email" placeholder="Email address" icon="mail"/>
+        <InputField type="password" placeholder="Password" icon="lock"/>
+        
+        <a href="#" className="forgot-pass-link">Forgot Password?</a>
+
+        <button className="login-button">Log In</button>
+      </form>
+      <p className="signup-text">Don&apos;t have an account? <a href="#"> Signup Now</a></p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SocialLogin.jsx
@@ -0,0 +1,18 @@
+const SocialLogin = () => {
+    return (
+        
+        <div className="social-Login">
+        <button className="social-button">
+            <img
+            src="Google.svg"
+            alt="Google"
+            className="social-icon"
+            />
+            Google
+        </button>
+        
+    </div>
+    )
+}
+
+export default SocialLogin
